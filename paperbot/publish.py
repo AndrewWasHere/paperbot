9aa2caa98355a963e450a5444fcb3fa5b6f14fce
Andrew Lin <andrew.lin@ieee.org>
2025-02-23 14:09:11 -0800

    Publishing to bluesky

--- paperbot/publish.py
+++ paperbot/publish.py
@@ -1,6 +1,8 @@
 import requests
 import tomllib
 
+from atproto import Client, client_utils
+
 from paperbot.common import Paper
 
 msg = "How about reading '{0.title}' @ {0.url} ?"
@@ -40,6 +42,28 @@ def to_discord(paper: Paper, credentials: str):
     print(f'`{content}` published to discord. resp={resp.status_code}')
 
 
+def bluesky_extract_from_credentials(path: str) -> tuple:
+    with open(path, 'rb') as f:
+        cfg = tomllib.load(f)
+
+    user = cfg['user']
+    pw = cfg['pw']
+
+    return user, pw
+
+
 def to_bluesky(paper: Paper, credentials: str):
     """Publish `paper` to bluesky using `credentials`."""
-    print(f'`{msg.format(paper)}` published to bluesky... Once this is implemented.')
+    user, pw = bluesky_extract_from_credentials(credentials)
+    content = client_utils.TextBuilder().text(
+        f"How about reading '{paper.title}' @ "
+    ).link(
+        paper.url, 
+        paper.url
+    ).text(
+        '?'
+    )
+    client = Client()
+    client.login(user, pw)
+    client.send_post(text=content)
+    print(f'`{msg.format(paper)}` published to bluesky')
